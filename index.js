3f4ef56406e0a0ff4d80723cf6662b10aef99d49
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-08-30 13:09:47 +0530

    final image api

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/api/image/random', async (req, res) => {
+    try {
+        const response = await axios.get("https://picsum.photos/200", { responseType: 'arraybuffer' });
+        res.set('Content-Type', 'image/jpeg'); 
+        res.send(response.data);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Error fetching random image');
+    }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
